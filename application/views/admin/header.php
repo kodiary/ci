7eceea926594d8222a9f233de6db8f11a9d82d8a
Anwar Ali <anwar@Anwars-MacBook-Pro.local>
2018-04-07 09:15:51 +0545

    uri segment

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -12,13 +12,15 @@
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
-
+	  <?php
+	  $uri = $this->uri->segment(2);
+	  ?>
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item active">
+	      <li class="nav-item <?php if($uri=='dashboard'){?>active<?php }?>">
 	        <a class="nav-link" href="<?php echo site_url('admin/dashboard')?>">Dashboard <span class="sr-only">(current)</span></a>
 	      </li>
-	      <li class="nav-item">
+	      <li class="nav-item <?php if($uri=='news'){?>active<?php }?>">
 	        <a class="nav-link" href="<?php echo site_url('admin/news');?>">News</a>
 	      </li>
 	      <li class="nav-item dropdown">
